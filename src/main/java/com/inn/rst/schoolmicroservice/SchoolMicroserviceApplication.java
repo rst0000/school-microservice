593f0c54102e808e01207270d5856382e914c70e
Ram Gopal Singh <ram.singh@rakuten.com>
2022-04-19 09:29:09 +0530

    initial commit

---
+++ src/main/java/com/inn/rst/schoolmicroservice/SchoolMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.inn.rst.schoolmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMicroserviceApplication.class, args);
+	}
+
+}
